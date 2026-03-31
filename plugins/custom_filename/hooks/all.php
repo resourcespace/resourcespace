19513f66a9bd924efbecf56d5b994374460bd09f
Toraih <git@urspace.ch>
2026-03-31 11:15:59 +0200

    Added option to keep filename-extension. The default behavior was not changed. 8 language-files manually updated (other languages were translated with ChatGPT)

--- plugins/custom_filename/hooks/all.php
+++ plugins/custom_filename/hooks/all.php
@@ -2,7 +2,7 @@
 
 function HookCustom_filenameAllUploadfilesuccess($resource_ref)
 {
-    global $filename_field, $cf_field;
+    global $filename_field, $cf_field, $cf_keep_extension;
 
     $filename = get_data_by_field($resource_ref, $filename_field);
 
@@ -11,12 +11,20 @@ function HookCustom_filenameAllUploadfilesuccess($resource_ref)
     }
 
     $filename_path_parts = pathinfo($filename);
-
-    if (trim($filename_path_parts['extension']) != '') {
-        $cf_errors = array();
-        update_field($resource_ref, $cf_field, $filename_path_parts['filename'], $cf_errors);
+    $filename_custom = trim($filename_path_parts['filename'] ?? '');
+    $filename_extension = trim($filename_path_parts['extension'] ?? '');
+    
+	if ($filename_extension == '' || $filename_custom == '') {
+		return;
+	}
+    
+	if ($cf_keep_extension && $filename_extension != '') {
+        $filename_custom .= ".{$filename_extension}";
     }
-
+    
+    $cf_errors = array();
+    update_field($resource_ref, $cf_field, $filename_custom, $cf_errors);
+    
     if (!empty($cf_errors)) {
         debug("CUSTOM_FILENAME - Uploadfilesuccess hook: Errors when updating field '{$cf_field}':");
         foreach ($cf_errors as $error) {
--- plugins/custom_filename/languages/ur.php
+++ plugins/custom_filename/languages/ur.php
@@ -2,5 +2,6 @@
 
 $lang["custom_filename_configuration"] = 'کسٹم فائل نام پلگ ان کی تشکیل';
 $lang["custom_filename_field_label"] = 'نئی قدر کہاں محفوظ کی جائے گی؟';
+$lang["custom_filename_keep_extension"] = 'فائل ایکسٹینشن برقرار رکھیں';
 
 $lang["page-title_custom_filename_setup"] = 'حسب ضرورت فائل کا نام پلگ ان ترتیب دیں';
